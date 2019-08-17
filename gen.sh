b6727ca38422330072c4d9687347acde607a2b2d
taotsi <tmkf.guthrie@gmail.com>
2019-08-17 11:50:36 +0800

    create dir

--- gen.sh
+++ gen.sh
@@ -1,16 +1,32 @@
 #!/bin/zsh
 
-if [ $# -ne 2 ]; then
-  echo "error: wrong number of arguments!"
-  echo "usage:\n  gen <project_name> <project_dir>"
-  exit 1
-fi
-
 proj_name=$1
 proj_dir=$2
 
-# TODO: test if project_dir exists already
+if [ $# -ne 2 ]; then
+  echo "error: wrong number of arguments!"
+  echo "usage:\n  gen <project name> <project dir>"
+  exit 1
+fi
+
+echo "Project is named as: $proj_name"
+echo "Project will be created in: $proj_dir"
+
+if [ -d $proj_dir ]; then
+  echo "$proj_dir already exists! exit now"
+  exit 1
+fi
+
 mkdir $proj_dir
+
+cp LICENSE $proj_dir
+
 cd $proj_dir
 
 touch CMakeLists.txt
+
+touch .gitignore
+echo build > .gitignore
+
+
+echo "cmake project "$proj_name" is successfully created!"
