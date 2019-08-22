854a2088903ea3d07ab8fca4bd458bf01995db47
taotsi <tmkf.guthrie@gmail.com>
2019-08-23 02:15:30 +0800

    remove redundant echos

--- gen.sh
+++ gen.sh
@@ -46,19 +46,22 @@ echo "# $proj_name" > README.md
 
 # CMakwLists.txt
 touch CMakeLists.txt
-echo "cmake_minimum_required(VERSION 3.7)" | tee -a CMakeLists.txt > /dev/null
-echo "project($proj_name VERSION 1.0 LANGUAGES CXX)" | tee -a CMakeLists.txt > /dev/null
-echo "add_subdirectory(src)" | tee -a CMakeLists.txt > /dev/null
+echo "cmake_minimum_required(VERSION 3.7)\n\
+project($proj_name VERSION 1.0 LANGUAGES CXX)\n\
+set(CMAKE_CXX_STANDARD 11)\n\
+set(CMAKE_CXX_EXTENSIONS OFF)\n\
+set(CMAKE_CXX_STANDARD_REQUIRED ON)\n\
+add_subdirectory(src)" | tee -a CMakeLists.txt > /dev/null
 
 cd src
 touch CMakeLists.txt
-echo "add_executable($exe_name main.cc)"  | tee -a CMakeLists.txt > /dev/null
-echo "target_include_directories($exe_name PUBLIC \${PROJECT_SOURCE_DIR}/extern/little-utility/include/little-utility)" | tee -a CMakeLists.txt > /dev/null
-echo "target_include_directories($exe_name PUBLIC \${PROJECT_SOURCE_DIR}/include/$proj_name)" | tee -a CMakeLists.txt > /dev/null
-echo "target_include_directories($exe_name PUBLIC \${PROJECT_SOURCE_DIR}/extern/Catch2/single_include/catch2)" | tee -a CMakeLists.txt > /dev/null
+echo "add_executable($exe_name main.cc)\n\
+target_include_directories($exe_name PUBLIC \${PROJECT_SOURCE_DIR}/extern/little-utility/include/little-utility)\n\
+target_include_directories($exe_name PUBLIC \${PROJECT_SOURCE_DIR}/include/$proj_name)\n\
+target_include_directories($exe_name PUBLIC \${PROJECT_SOURCE_DIR}/extern/Catch2/single_include/catch2)\n\
+set_target_properties($exe_name PROPERTIES RUNTIME_OUTPUT_DIRECTORY \${PROJECT_BINARY_DIR})" | tee -a CMakeLists.txt > /dev/null
 
-echo "set_target_properties($exe_name PROPERTIES RUNTIME_OUTPUT_DIRECTORY \${PROJECT_BINARY_DIR})" | tee -a CMakeLists.txt > /dev/null
-touch $exe_name.cc
+touch main.cc
 echo "#include \"utility.hh\"\n\nint main(int argc, const char** argv)\n{\n  \n  return 0;\n}" | tee $exe_name.cc > /dev/null
 cd ..
 # TODO: build test
